7d1193262246fa5c9810e0625828e0e36a1cccaf
Sergey Zagoruyko <zagoruyko2@gmail.com>
2016-02-18 17:25:03 +0100

    use torch trained nin

--- imagenet_classification/demo.lua
+++ imagenet_classification/demo.lua
@@ -3,7 +3,6 @@ require 'cv.highgui'
 require 'cv.videoio'
 require 'cv.imgproc'
 require 'nn'
-require 'loadcaffe'
 
 local cap = cv.VideoCapture{device=0}
 if not cap:isOpened() then
@@ -14,36 +13,30 @@ end
 cv.namedWindow{winname="torch-OpenCV ImageNet classification demo", flags=cv.WINDOW_AUTOSIZE}
 local _, frame = cap:read{}
 
--- Setting up network
-proto_name = 'deploy.prototxt'
-model_name = 'nin_imagenet.caffemodel'
-img_mean_name = 'ilsvrc_2012_mean.t7'
-
-prototxt_url = 'http://git.io/vIdRW'
-model_url = 'https://www.dropbox.com/s/0cidxafrb2wuwxw/'..model_name
-img_mean_url = 'https://www.dropbox.com/s/p33rheie3xjx6eu/'..img_mean_name
-
-if not paths.filep(proto_name) then os.execute('wget '..prototxt_url..' -O '..proto_name) end
-if not paths.filep(model_name) then os.execute('wget '..model_url)    end
-if not paths.filep(img_mean_name) then os.execute('wget '..img_mean_url) end
+print '==> Downloading image and network'
+local image_url = 'http://upload.wikimedia.org/wikipedia/commons/e/e9/Goldfish3.jpg'
+local network_url = 'https://www.dropbox.com/s/npmr5egvjbg7ovb/nin_nobn_final.t7'
+local image_name = paths.basename(image_url)
+local network_name = paths.basename(network_url)
+if not paths.filep(image_name) then os.execute('wget '..image_url)   end
+if not paths.filep(network_name) then os.execute('wget '..network_url)   end
 
 
 print '==> Loading network'
 -- Using network in network http://openreview.net/document/9b05a3bb-3a5e-49cb-91f7-0f482af65aea
-local net = loadcaffe.load(proto_name, './nin_imagenet.caffemodel'):float()
-local img_mean = torch.load'ilsvrc_2012_mean.t7'.img_mean:float()
+local net = torch.load(network_name):unpack():float()
 local synset_words = torch.load('synset.t7','ascii')
 
-local M = 227
+local M = 224
 
 while true do
    local w = frame:size(2)
    local h = frame:size(1)
 
    local crop = cv.getRectSubPix{image=frame, patchSize={h,h}, center={w/2, h/2}}
-   local im = cv.resize{src=crop, dsize={256,256}}
-   local im2 = im:float() - img_mean
-   local I = cv.resize{src=im2, dsize={M,M}}:permute(3,1,2):clone()
+   local im = cv.resize{src=crop, dsize={256,256}}:float():div(255)
+   for i=1,3 do im:select(3,i):add(-net.transform.mean[i]):div(net.transform.std[i]) end
+   local I = cv.resize{src=im, dsize={M,M}}:permute(3,1,2):clone()
 
    local _,classes = net:forward(I):view(-1):float():sort(true)
 

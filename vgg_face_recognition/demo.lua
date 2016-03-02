8f66100807c241bc7403e85ad70a7817c49eca1c
Egor Burkov <shrubb@ya.ru>
2016-03-02 12:36:56 +0300

    Mouse callback, recognition phase

--- vgg_face_recognition/demo.lua
+++ vgg_face_recognition/demo.lua
@@ -5,8 +5,14 @@ require 'cv.cudaobjdetect' -- CascadeClassifier
 require 'cv.cudawarping'   -- resize
 require 'cv.cudaimgproc'   -- cvtColor
 require 'cv.imgproc'       -- rectangle
+cv.ml = require 'cv.ml'    -- SVM
+
+require 'cutorch'
 require 'nn'
 
+-------------------------------------------------------------------------------
+-- Describe command line arguments
+-------------------------------------------------------------------------------
 if not arg[1] or not arg[2] then
     print[[
 Usage: th demo.lua P N [Name1 Name2 ...]
@@ -19,20 +25,33 @@ Where
     os.exit(-1)
 end
 
+-------------------------------------------------------------------------------
+-- Set up machine learning models
+-------------------------------------------------------------------------------
+-- Viola-Jones face detector
 local faceDetector = cv.cuda.CascadeClassifier{arg[1]}
 
+-- Convolutional neural network face descriptor by VGG
 print('Loading the network...')
 local network = torch.load('./VGG_FACE.t7')
 network:evaluate()
 
+-- SVM to classify descriptors in recognition phase
+local svm = cv.ml.SVM{}
+svm:setType         {cv.ml.SVM_C_SVC}
+svm:setKernel       {cv.ml.SVM_LINEAR}
+svm:setDegree       {2}
+svm:setTermCriteria {{cv.TermCriteria_MAX_ITER, 100, 1e-6}}
+
+-------------------------------------------------------------------------------
+-- Set up video stream and GUI, unpack input arguments
+-------------------------------------------------------------------------------
 local capture = cv.VideoCapture{device=0}
 assert(capture:isOpened(), 'Failed to open the default camera')
 
 cv.namedWindow{'Stream window'}
 cv.namedWindow{ 'Faces window'}
 cv.setWindowTitle{'Faces window', 'Grabbed faces'}
-
--- **************************  Labeling the data **************************
 
 local N = assert(tonumber(arg[2]))
 
@@ -55,12 +74,12 @@ end
 
 updateFaceNumber(1)
 
-local faceSamples = {}
-for i = 1,N do faceSamples[i] = {} end
+local faces = {}
+for i = 1,N do faces[i] = {} end
 
-local function enoughFaceSamples()
+local function enoughFaces()
     local minNumber = 1e9
-    for i = 1,N do minNumber = math.min(minNumber, #faceSamples[i]) end
+    for i = 1,N do minNumber = math.min(minNumber, #faces[i]) end
     return minNumber >= 2
 end
 
@@ -70,8 +89,28 @@ local frameCUDA = torch.CudaTensor(frame:size())
 local scaleFactor = 0.5
 local frameCUDAGray = torch.CudaTensor((#frame)[1] * scaleFactor, (#frame)[2] * scaleFactor)
 
--- Labeling loop
-while stillLabeling do
+local faceRects
+
+local function onMouse(event, x, y, flags)
+    if not faceRects or event ~= cv.EVENT_LBUTTONDBLCLK then
+        return
+    end
+
+    print(x, y)
+    -- find matching rectangle from faceRects
+    -- crop it
+    -- resize as needed
+    -- pass it forward through CNN
+    -- get a descriptor
+    -- insert it into faces[currentFaceNumber]
+end
+
+cv.setMouseCallback{'Stream window', onMouse}
+
+-------------------------------------------------------------------------------
+-- The main loop
+-------------------------------------------------------------------------------
+while true do
     if not pause then
         -- upload image to GPU and normalize it from [0..255] to [0..1]
         frameCUDA:copy(frame):div(255)
@@ -81,13 +120,13 @@ while stillLabeling do
         cv.cuda.resize{frameCUDA:select(3,1), dst=frameCUDAGray, fx=scaleFactor, fy=scaleFactor}
         
         -- detect faces in downsampled image
-        local faces = faceDetector:detectMultiScale{frameCUDAGray}
+        faceRects = faceDetector:detectMultiScale{frameCUDAGray}
         -- convert faces to RectArray from OpenCV-CUDA's internal representation
-        faces = faceDetector:convert{faces}
+        faceRects = faceDetector:convert{faces}
         
         -- draw faces
-        for i = 1,faces.size do
-            local f = faces.data[i]
+        for i = 1,faceRects.size do
+            local f = faceRects.data[i]
             -- translate face coordinates to the original big image
             f.x      = f.x      / scaleFactor
             f.y      = f.y      / scaleFactor
@@ -100,23 +139,39 @@ while stillLabeling do
         end
     end
 
-    cv.imshow{'Stream window', frame}
-   
     local key = cv.waitKey{20}
 
-    if key >= 49 and key <= 57 then
-        -- key is a digit: change current number of face to be labeled
-        updateFaceNumber(key-48)
-    elseif key == 32 then
-        -- key is Space  : set pause
-        pause = not pause
-    elseif key == 13 then
-        -- key is Enter  : end labeling if there are at least 2 samples for each face
-        if enoughFaceSamples() then stillLabeling = false end
-    elseif key == 27 then
-        -- key is Esc    : quit
-        os.exit(0)
+    if stillLabeling then
+        -- labeling phase
+        if key >= 49 and key <= 57 then
+            -- key is a digit: change current number of face to be labeled
+            updateFaceNumber(key-48)
+        elseif key == 32 then
+            -- key is Space  : set pause
+            pause = not pause
+        elseif key == 13 then
+            -- key is Enter  : end labeling if there are at least 2 samples for each face
+            if enoughfaces() then stillLabeling = false end
+        elseif key == 27 then
+            -- key is Esc    : quit
+            os.exit(0)
+        end
+    else
+        -- recognition phase
+
+        for i = 1,faceRects.size do
+            local f = faceRects.data[i]
+
+            -- crop the face
+            -- resize as needed
+            -- pass forward through CNN
+            -- get descriptor
+            -- feed it to SVM, get class prediction
+            -- draw predicted name above the rectangle
+        end
     end
+
+    cv.imshow{'Stream window', frame}
 
     if not pause then capture:read{frame} end
 end

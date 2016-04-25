1c296e0433fab6ec83fe8b2f395eac34a1a5a1b6
Sergey Zagoruyko <zagoruyko2@gmail.com>
2016-04-25 12:28:44 +0200

    texture nets foldBatchNorm

--- texture_nets/stylization.lua
+++ texture_nets/stylization.lua
@@ -6,6 +6,7 @@ require 'cv.imgproc'
 require 'nn'
 require 'SpatialCircularPadding'
 require 'NoiseFill'
+local utils = require 'inn.utils'
 
 require 'image'
 require 'nn'
@@ -19,7 +20,7 @@ local opt = xlua.envparams{
 }
 
 
-local cap = opt.video_path == '' and cv.VideoCapture{device=0} or cv.VideoCapture{filename=opt.video_path}
+local cap = cv.VideoCapture{opt.video_path ~= '' and opt.video_path or 0}
 if not cap:isOpened() then
     print("Failed to open the default camera")
     os.exit(-1)
@@ -40,8 +41,12 @@ local function cast(x)
    end
 end
 
-local net = torch.load(opt.network)
-cast(net):evaluate()
+-- yes, that's true, net takes 0-1 input and output is VGG
+local function preprocess(frame)
+   local frame1 = frame:permute(3,1,2):float() / 255
+   frame1 = image.scale(frame1, opt.frame_height)
+   return frame1:view(1,table.unpack(frame1:size():totable()))
+end
 
 local function deprocess(img)
   local mean_pixel = torch.FloatTensor({103.939, 116.779, 123.68})
@@ -49,13 +54,30 @@ local function deprocess(img)
   return img
 end
 
+local net = torch.load(opt.network)
+cast(net):evaluate()
+
+-- to enable foldBatchNorm, otherwise it cannot detect conv+BN pairs
+local function simplify(net)
+   for i,v in ipairs(net:findModules'nn.Sequential') do
+      for j,u in ipairs(v.modules) do
+         if torch.typename(u) == 'nn.Sequential' and u.modules and #u.modules == 2 then
+            v:remove(j)
+            v:insert(u:get(1),j)
+            v:insert(u:get(2),j+1)
+         end
+      end
+   end
+end
+
+local input = cast(preprocess(frame))
+utils.testSurgery(input, simplify, net)
+utils.testSurgery(input, utils.foldBatchNorm, net)
+print(net)
+
 
 while true do
-    local frame1 = frame:permute(3,1,2):float() / 255
-    frame1 = image.scale(frame1, opt.frame_height)
-
-    local input = frame1:view(1,table.unpack(frame1:size():totable()))
-    
+    local input = preprocess(frame)
     local out = deprocess(net(cast(input))[1]:float())
     cv.imshow{opt.network, (torch.clamp(out:permute(2,3,1),0,255)):byte()}
 
